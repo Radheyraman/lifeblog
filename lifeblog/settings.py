e544310e730b9f17131820265f37f5b907597c91
radheyraman <p.radheyraman2930@gmail.com>
2023-09-27 19:40:08 +0530

    vercel problem2

--- lifeblog/settings.py
+++ lifeblog/settings.py
@@ -12,9 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4xi1w6+77%dx2##b+l8zsn3*@ki=#!%hc&^75lf_d3@91&go!u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app']
+# ALLOWED_HOSTS = []
 
 
 # Application definition
